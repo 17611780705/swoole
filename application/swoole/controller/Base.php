48a14129eba5ff9160737c2dec72551037f0ef50
liuzhu <liuzhu@zanpu.com>
2018-09-19 14:40:59 +0800

    swoole

---
+++ application/swoole/controller/Base.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2018/9/19
+     * Time: 14:40
+     */
+
+namespace app\swoole\controller;
+
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
---
+++ application/swoole/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/19
+ * Time: 14:39
+ */
+
+namespace app\swoole\controller;
+
+class Index extends Base
+{
+    public function index()
+    {
+        echo 1111;
+    }
+}
